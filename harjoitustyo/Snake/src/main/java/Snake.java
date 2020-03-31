6863d3e645798cdb3b5516e1bbf693882d86e172
osma <okovaskainen@evo.paivola.fi>
2020-03-31 20:15:25 +0300

    started project

---
+++ harjoitustyo/Snake/src/main/java/Snake.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author osma
+ */
+public class Snake {
+    int size;
+    public Snake(){
+        this.size = 1;
+    }
+    public void Eat() {
+        this.size++;
+    }
+}
---
+++ harjoitustyo/Snake/src/test/java/SnakeTest.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+/**
+ *
+ * @author osma
+ */
+public class SnakeTest {
+
+    public SnakeTest() {
+    }
+
+    @BeforeAll
+    public static void setUpClass() {
+    }
+
+    @AfterAll
+    public static void tearDownClass() {
+    }
+
+    @BeforeEach
+    public void setUp() {
+    }
+
+    @AfterEach
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    @Test
+    public void eatTest() {
+        Snake snake = new Snake();
+        assertEquals(1, snake.size);
+        snake.Eat();
+        assertEquals(2, snake.size);
+
+    }
+}
